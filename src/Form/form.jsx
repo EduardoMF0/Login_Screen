22bc6a88f195483576d53d826cfc0361084d2031
DESKTOP <DESKTOP@DESKTOP-BCUDA68>
2025-02-17 10:28:22 -0300

    arranging elements

--- src/Form/form.jsx
+++ src/Form/form.jsx
@@ -4,22 +4,24 @@ function Form(){
 
     return(
         <div className="box1">
-            <h1>Acesse Sua Conta</h1>
-            <h2>Facilite sua compra logando na sua conta</h2>
+
+            <section className='title'>
+                <h1>Acesse Sua Conta</h1>
+                <h2>Facilite sua compra logando na sua conta</h2>
+            </section>
 
             <section>
                 <p>Email:</p>
                 <input type="text" />
-            </section>
 
-            <section>
                 <p>Senha:</p>
                 <input type="text" />
+
+                <a href=''>Esqueceu sua Senha?</a>
             </section>
 
-            <a href=''>Esqueceu sua Senha?</a>
-
             <button type='submit'>Entrar</button>
+
         </div>
 
     );
