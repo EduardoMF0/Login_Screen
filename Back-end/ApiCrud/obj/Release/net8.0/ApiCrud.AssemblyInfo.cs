a22c638a828e3bea56446fc2949c5b56f7dfc799
DESKTOP <159490289+EduardoMF0@users.noreply.github.com>
2025-02-27 23:41:09 -0300

    Change files

--- Back-end/ApiCrud/obj/Release/net8.0/ApiCrud.AssemblyInfo.cs
+++ Back-end/ApiCrud/obj/Release/net8.0/ApiCrud.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCrud")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a991dfc5fc754b00ee861041c03e5334df8a66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCrud")]
