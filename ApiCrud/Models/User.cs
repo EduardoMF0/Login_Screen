5f7a1e6077298841b9c7a61bdea769d0b61c6907
DESKTOP <159490289+EduardoMF0@users.noreply.github.com>
2025-03-05 21:52:15 -0300

    fix connection and attibute

---
+++ ApiCrud/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCrud.Models
+{
+    [Table("usuarios_registrados")]
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; private set; }
+
+        public string nome { get; set; }
+
+        public string email { get; set; }
+
+        public string senha { get;  set; }
+
+        public string estado { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime dta_nascimento { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime dta_criacaoconta { get; set; }
+
+        public User(string nome, string email, string senha,string estado, DateTime dta_nascimento)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+            this.estado = estado;
+            this.dta_nascimento = dta_nascimento;
+            dta_criacaoconta = DateTime.UtcNow;
+        }
+    }
+}
---
+++ ApiCrud/Models/UserAdd.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCrud.Models
+{
+    [Table("usuarios_registrados")]
+    public class UserAdd
+    {
+        public long id { get; private set; }
+
+        public string nome { get; set; }
+
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
+        public string estado { get; set; }
+
+        public DateTime dta_nascimento { get; set; }
+
+        public DateTime dta_criacaoconta { get; set; }
+        public UserAdd()
+        {
+
+        }
+    }
+}
